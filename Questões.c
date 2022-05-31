4a6147b0f33d4b64f195bbe7d696ae4e2a8f6334
Pedro Costard oliveira <100092162+Costards2@users.noreply.github.com>
2022-05-31 20:17:34 -0300

    Update Questões.c

--- Questões.c
+++ Questões.c
@@ -1349,6 +1349,53 @@ int main()
     printf("A proposta B teve %i \n", B);
     printf("A proposta C teve %i \n", C);
     }
+/*60. A famosa conjectura de Goldbach diz que todo inteiro par maior que 2 é soma de dois 
+números primos. Testes foram feitos, mas ainda não se achou um contra-exemplo. 
+Escreva um programa mostrando que a afirmação é verdadeira para todo número par 
+entre 500 e 1000. O programa deve imprimir o número par e os dois primos que somados 
+dão o número par.*/
+ int main()
+ {
+  int iCont;
+	int jCont;
+	int kCont;
+	int termo1;
+	int divTermo1;
+	int termo2;
+	int divTermo2;
+	int numero;
+	int naoAchei;
+	
+	
+	for(kCont = 4; kCont <= 5000; kCont++){
+		numero = kCont;
+		if(numero % 2 == 0){
+			for(iCont = 1, naoAchei = 1; iCont <= numero; iCont++){
+			  divTermo1 = 0;	
+			  for(termo1 = 1; termo1 < iCont && naoAchei == 1;  termo1++)
+			    if(iCont % termo1 == 0)
+			      divTermo1++;
+		   	  if(divTermo1 == 1){
+				for(jCont = 1; jCont <= numero; jCont++){
+				  divTermo2 = 0;	
+				  for(termo2 = 1; termo2 < jCont && naoAchei == 1;  termo2++)
+				    if(jCont % termo2 == 0)
+				      divTermo2++;
+			   	  if(divTermo2 == 1){
+			   	  	if(termo1 + termo2 == numero){
+			   	  	  printf("%d + %d = %d\n", termo1, termo2, numero);
+			   	  	  naoAchei = 0;
+			   		}
+			      }
+		       }
+			  }
+			}
+			if(naoAchei == 1){
+				printf("CONJECTURA FALHOU PARA %d\n", numero);
+			}
+		}
+	}
+}
 
  
 
