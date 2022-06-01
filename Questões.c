a00ea7c60751c83d5609b92eb18ea0472b0bef97
Pedro Costard oliveira <100092162+Costards2@users.noreply.github.com>
2022-06-01 16:09:15 -0300

    Update Questões.c

--- Questões.c
+++ Questões.c
@@ -1418,7 +1418,9 @@ dão o número par.*/
 		}
 	}
 }
-
+/*61. Faça um programa em C que crie e inicialize um array de 20 posições de inteiros com 0 
+para cada elemento. Imprima o vetor em seguida, indicando a posição e o valor na 
+posição (um por linha).*/
  
 
    
